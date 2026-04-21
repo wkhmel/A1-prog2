8ff0f83b406de04d5dd7a528479716074a71b12b
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2026-04-21 00:12:44 -0300

    funções aux do gbv_order

--- gbv.c
+++ gbv.c
@@ -400,6 +400,30 @@ int gbv_view(const Library *lib, const char *archive, const char *docname) {
     return 0;
 }
 
+int ordem_alfabetica(const void *a, const void *b) {
+    /*convertendo o ponteiro do tipo void (generico pra comparação) p tipo Document */
+    const Document *docA = (const Document *)a;
+    const Document *docB = (const Document *)b;
+
+    /* dá zero se forem iguais, valor negativo se o primeiro for menor q o segundo e positivo caso contrario */
+    return strcmp(docA->name, docB->name);
+}
+
+int ordem_tamanho(const void *a, const void *b) {
+    const Document *docA = (const Document *)a;
+    const Document *docB = (const Document *)b;
+
+    /* subtrai do valor que tem mais bytes o valor que tem menos bytes */
+    return (docA->size > docB->size) - (docA->size < docB->size);
+}
+
+int ordem_cronologica(const void *a, const void *b) {
+    const Document *docA = (const Document *)a;
+    const Document *docB = (const Document *)b;
+    /* retorna a diferença de data (do que é mais recente para o mais antigo)*/
+    return (docA->date > docB->date) - (docA->date < docB->date);
+}
+
 int gbv_order(Library *lib, const char *archive, const char *criteria) {
     if (!lib || !criteria) {
         fprintf(stderr, "Ponteiro nulo.\n");
